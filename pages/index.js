c273927ccbff475571eb763449d66fce3c193c68
Brian Smawley <briansmawley@gmail.com>
2023-02-28 15:22:03 -0500

    Formatting changes and blog

--- pages/index.js
+++ pages/index.js
@@ -109,9 +109,9 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="mt-10 laptop:mt-30 p-2 laptop:p-0">
+        <div className="mt-10 laptop:mt-40 p-2 laptop:p-0">
           <h1 className="tablet:m-10 text-4xl text-bold">Services.</h1>
-          <div className="mt-5 tablet:m-10 grid grid-cols-1 laptop:grid-cols-2 gap-6">
+          <div className="tablet:m-10 mt-2 text-xl laptop:text-3xl w-full laptop:w-3/5">
             {data.services.map((service, index) => (
               <ServiceCard
                 key={index}
